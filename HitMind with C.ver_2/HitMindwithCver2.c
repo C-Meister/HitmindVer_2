2279f077b93ea3ece6183970a42d1c0913b6a275
unknown <>
2017-09-13 21:02:05 +0900

    초기 커밋.

---
+++ HitMind with C.ver_2/HitMindwithCver2.c
@@ -0,0 +1,16 @@
+﻿/*
+HitMind With C.ver_2
+조원: 킹상호, 갓상희, 엠퍼럴수한, 제너럴민석
+콘솔따윈없다
+
+*/
+
+#include <stdio.h>
+#include "mysql/mysql.h"
+#pragma comment(lib, "lib\\libmysql.lib")
+int main()
+{ 
+	mysql_init(NULL);
+	return 0;
+}
+
