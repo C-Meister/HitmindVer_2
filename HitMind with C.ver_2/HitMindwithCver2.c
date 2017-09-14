942c918c78b501b6645e248440edb432399ef7b7
서상희 <tbvjaos510@naver.com>
2017-09-14 14:42:26 +0900

    Hello World

--- HitMind with C.ver_2/HitMindwithCver2.c
+++ HitMind with C.ver_2/HitMindwithCver2.c
@@ -6,10 +6,27 @@ HitMind With C.ver_2
 */
 
 #include <stdio.h>
+
+#include "SDL/SDL.h"			//SDL - 그래픽 헤더파일
+#include "SDL/SDL_image.h"		
+#include "SDL/SDL_ttf.h"
+#include "SDL/han2unicode.h"
 #include "mysql/mysql.h"
-#pragma comment(lib, "lib\\libmysql.lib")
-int main()
-{ 
+#include "SDL/SDL_mixer.h"
+#include "SDL/SDL.h"
+#include "lib/iconv.h"
+#define nullptr 0
+// 라이브러리 선언문 라이브러리파일은 따로 추가안해도 됩니다.
+// #pragma comment 는 visual studio에서만 사용 가능 *솔루션 플렛폼을 64비트로 해주세요
+#pragma comment (lib, "lib/libmysql.lib")	//mysql라이브러리
+#pragma comment (lib, "lib/SDL2")			//그래픽 라이브러리 1
+#pragma comment (lib, "lib/SDL2main")		//그래픽 라이브러리 2
+#pragma comment (lib, "lib/SDL2_image")		//그래픽 라이브러리 3
+#pragma comment (lib,"lib/SDL2_ttf")
+#pragma comment (lib, "lib/SDL2_mixer.lib")	//그래픽 사운드 라이브러리 4
+int main(int argc, char *argv[])
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
 	mysql_init(NULL);
 	return 0;
 }
