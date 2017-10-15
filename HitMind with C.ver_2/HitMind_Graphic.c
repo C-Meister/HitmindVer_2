f3cc4d4cb65511452d4e3a41957452f6b9750bf6
서상희 <tbvjaos510@naver.com>
2017-10-15 10:24:06 +0900

    sdf

--- HitMind with C.ver_2/HitMind_Graphic.c
+++ HitMind with C.ver_2/HitMind_Graphic.c
@@ -1564,6 +1564,6 @@ void PrintNotice(SDL_Renderer * renderer, MYSQL*cons)
 		printf("%s\n", notice[j].content);
 		
 		plus_y += PutText_ln(NULL, Display_X * 0.37, Display_Y * 0.34, Display_Y * 0.4, renderer, notice[j].content, Display_X * 0.32, Display_Y * 0.4 + plus_y,30 * ((float)Display_X / 1920), 0, 0, 0, 1);
-		plus_y += 10;
+		plus_y += 8;
 	}
 }
